ba000ac4f8a4f506da1133f502aab458e394651a
Dev-Iyke <iyke.thompson01@gmail.com>
2025-01-19 19:20:20 +0100

    Added a try finally block to ensure user enters the right values

--- main.py
+++ main.py
@@ -1,38 +1,33 @@
-def add_to_cart(item_name, price, *args):
-  # print(kwargs)
+cart = []
+
+def add_to_cart(item_name, price, *args, **kwargs):
+  print(kwargs)
   item_to_add = {
     'name': item_name,
     'price': float(price),
-    'discount': args[0],
-    # 'properties': {
-    #   'color': kwargs[color],
-    #   'size': kwargs[size],
-    # }
+    'discount': float(args[0]),
+    'size': kwargs['size'],
+    'color': kwargs['color'],
+    'final_price': price - (price * (args[0]/100))
   }
   print(item_to_add)
+  cart.append(item_to_add)
+  print(f'Added {item_name} to the cart')
+  print(cart)
 
-cart = {}
+
 while True:
-  item_name = input('Enter item name or q to exit')
+  item_name = input('Enter item name or q to exit: ')
   if item_name == 'q':
     break
-  item_price = input('Enter item price or q to exit')
-  if item_price == 'q':
-    break
-  item_discount = input('Enter item discount or q to exit')
-  if item_discount == 'q':
-    break
-  item_to_add = {
-    'name': item_name,
-    'price': float(item_price),
-    'discount': float(item_discount),
-  }
-  cart[item_name] = item_to_add
-  print(f'Added {item_name} to the cart')
+  else:
+    try:
+      item_price = int(input('Enter item price: '))
+      item_discount = int(input('Enter item discount: '))
+      item_size = input('Enter item size: ')
+      item_color = input('Enter item color: ')
+    finally:
+      add_to_cart(item_name, item_price, item_discount, size=item_size, color=item_color)
+
 
 print('The end')
-print(cart)
-# add_to_cart(name, price, discount)
-
-
-# print(item_to_add)
