935b9242a8dbea04438f9d3798f5ee151acc6930
Dev-Iyke <iyke.thompson01@gmail.com>
2025-01-21 11:54:47 +0100

    Added a function to check if the item exists before adding to cart and to get summary

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+# Initialized the cart as an empty list
 cart = []
 
-def add_to_cart(item_name, price, *args, **kwargs):
-  print(kwargs)
+def add_to_cart(item_name, price, *args, **kwargs) -> dict:
+  """
+  This is a function that takes the details of an item and add it to the cart
+  parameters
+  item_name: str, the name of the item
+  price: float, the price of the item
+  *args: tuple, additional arguments for the item (discount)
+  **kwargs: dict, keyword arguments for the item (size, color)
+
+  returns:
+  List[dict], the updated cart with the new item added
+  """
+
   item_to_add = {
     'name': item_name,
     'price': float(price),
@@ -10,15 +22,20 @@ def add_to_cart(item_name, price, *args, **kwargs):
     'color': kwargs['color'],
     'final_price': price - (price * (args[0]/100))
   }
-  print(item_to_add)
+
+  # Check if the item already exists in the cart, if so, print a message and return the cart without adding the new item
+  for item in cart:
+    if (item['name'] == item_to_add['name']):  
+      print(f'{item_name} already exists in the cart')
+      return
   cart.append(item_to_add)
   print(f'Added {item_name} to the cart')
-  print(cart)
+  return cart
 
-
+#  Allows the user to add multiple items to the cart. 
 while True:
-  item_name = input('Enter item name or q to exit: ')
-  if item_name == 'q':
+  item_name = input('Enter item name or done to finish: ')
+  if item_name == 'done':
     break
   else:
     try:
@@ -29,5 +46,24 @@ while True:
     finally:
       add_to_cart(item_name, item_price, item_discount, size=item_size, color=item_color)
 
+
+def get_summary() -> float:
+  """
+  This function calculates and prints the total cost of all items in the cart
+  
+  returns:
+  float, the total cost of all items in the cart
+  """
+  total = 0
+  for item in cart:
+    print(f"{item['name']} - ${item['final_price']} (color = {item['color']}, size = {item['size']})")
+    total += item['final_price']
+
+  print(f'Cost: {total}')
+  return total
+
+# Check if the cart is empty before printing the summary
+if cart:
+  get_summary()
 
 print('The end')
